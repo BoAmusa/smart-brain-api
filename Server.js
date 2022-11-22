064b62b8858cf2ac0ac4bc2dc8d60a04ac433173
Bo Amusa <Boamusa@gmail.com>
2022-11-21 22:50:17 -0500

    adding index

--- Server.js
+++ Server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Connected!!");
+  res.sendFile("index.html");
 });
 
 app.get("/profile/:id", (req, res) => {
